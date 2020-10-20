9fb52fbd3a415320e1f42833b742783186d82842
Alan S Mathew <alansmathew@Icloud.com>
2020-10-20 23:03:17 +0530

    user information accept wating 🤤

--- getSchedule.py
+++ getSchedule.py
@@ -13,10 +13,31 @@ def log(str, *args, wait=False, **kwargs):
 def main():
 
     browser = webdriver.Chrome(input("drag in your Chrome driver : "))
+    # browser = webdriver.Chrome(executable_path = '/Users/alansmathew/Desktop/getSchedule/chromedriver')
+
+    # this function is used to wait for certain time to get the page loaded 
+    # !note if no connection or page failed to load, after certain recersion the function will quit itself 
+    def loading(classname):
+        try:
+            browser.find_element_by_class_name(classname)
+        except:
+            time.sleep(1)
+            loading(classname)
+
 
     #LOAD AJCE LOGIN PORTAL
-    browser.get('http://www.ajce.in/') #not sure about urls
+    browser.get('http://www.aesajce.in/')
+    time.sleep(3)
+
     print('Waiting for user to log in')
+    loading('active-sub')
+    print('Accessing information')
+    time.sleep(1)
+    # browser.get('http://www.aesajce.in/zoomschedule.php')
+    # print('done')
+    # time.sleep(20)
+
+    
 
     #Detect login to continue to daily schedule
 
@@ -28,4 +49,4 @@ if __name__=='__main__':
     try:
         main()
     except Exception as e: log(e)
-    finally: browser.quit()
+    # finally: browser.quit()
