129514185b202721a43076f21dae373d1a69d386
Jitin J Gigi <jitinjacobgigi@mca.ajce.in>
2020-10-22 04:09:34 +0530

    THE DEED IS DONE
    requires debugs on mac devices as well

--- getSchedule.py
+++ getSchedule.py
@@ -1,8 +1,7 @@
 import time 
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 TESTING = True
 def log(str, *args, wait=False, **kwargs):
@@ -11,44 +10,85 @@ def log(str, *args, wait=False, **kwargs):
         print(str, *args, **kwargs)
         if wait:
             input()
-
-def main():
-
-    browser = webdriver.Chrome(input("drag in your Chrome driver : "))
-    # browser = webdriver.Chrome(executable_path = '/Users/alansmathew/Desktop/< support file /chromedriver')
-  
-    #LOAD AJCE LOGIN PORTAL
-    browser.get('http://www.aesajce.in/')
-    time.sleep(3)
-
-    print('Waiting for user to log in')
-    WebDriverWait(browser, 60*3).until(lambda driv:driv.find_elements(By.CLASS_NAME, 'active-sub'))
-    print('Accessing information')
-    time.sleep(1)
-    browser.find_element_by_xpath("//a[contains(@href,'zoomschedule.php')]").click()
-    print("Login Sucessfull")
     
-    #Detect login to continue to daily schedule
 
-    #Obtain schedule into list/dict
-    print("Obtaining data")
-    table=browser.find_element_by_id('demo-dt-addrow')  #getting table object
-    data_object = table.find_elements_by_tag_name('td') #getting table data objects (td) inside table
-    data= [str(data.text) for data in data_object if data.text != '']   #converting / getting data using table data objects
-    print(data) #list of datas
+try:
+#    path = input("drag in your Chrome driver : ")
+#    path = '/Users/alansmathew/Desktop/< support file /chromedriver'
+    path = 'D:\\SETUPS\\chromedriver.exe'
+    chrome_options = Options()
+    chrome_options.add_argument('--log-level=3')
 
-    time.sleep(1)
-
-    table_link=browser.find_elements_by_xpath("//a[contains(@title,'room')]")
-    link=[str(link.get_attribute('href')) for link in table_link if link.get_attribute('href') != '']
-    print(link) #list of links
-
-    #process data
-
-
-
-if __name__=='__main__':
     try:
-        main()
-    except Exception as e: log(e)
-    # finally: browser.quit()
+        exec(open('cookies.txt','r').read())
+    except IOError:
+        cookiefile = input("Drag and Drop your cookies.txt file here.\nIf you don't have your cookies file, you'll have to press enter to create one.\nThis will ask you to log in and solve a bunch of CAPTCHAS.\ncookies.txt or Enter : ")
+        if cookiefile != '':
+            exec(open(cookiefile, 'r').read())
+        else:
+            browser = webdriver.Chrome(
+                executable_path = path,
+                options = chrome_options
+            )
+            browser.get("http://www.aesajce.in")
+            input('Press Enter here after Loging into your account.')
+            browser.get("https://www.aesajce.in/students/zoomschedule.php")
+            open("cookies.txt","w").write('global cookies; cookies = '+str(browser.get_cookies()))
+            cookies = browser.get_cookies()
+            print("cookies downloaded. Yay")
+            browser.close()
+            
+    except Exception as e: 
+        log(e);exit()
+
+    browser = webdriver.Chrome(
+        executable_path = path,
+        options = chrome_options
+    )
+    
+    log('loading page')
+    browser.get('https://www.aesajce.in/')
+    log('loading cookies')
+    for cookie in cookies:
+        browser.add_cookie(cookie)
+    browser.get('https://www.aesajce.in/students/zoomschedule.php')
+    browser.get('https://www.aesajce.in/students/zoomschedule.php')
+
+    log("obtaining table")
+    table = (browser
+        .find_element_by_tag_name('table')
+        .find_element_by_tag_name('tbody')
+    )
+    log('obtaining link elements')
+    link = table.find_elements_by_tag_name("a")
+    log('obtaining details')
+    details = [
+        [
+            td.text
+            for td in tr.find_elements_by_tag_name('td')
+            if td.text != ''
+        ]
+        for tr in table.find_elements_by_tag_name('tr')
+    ]
+    log('obtaining realinks')
+    links=[
+        str(a.get_attribute('href'))
+        for a in link
+    ]
+    log('Cooking Data\n')
+    data = [
+        detail[1:-1]+[link]
+        for detail, link in zip(details, links)
+    ]
+    
+    browser.close()
+    for subject in data:
+        print(*subject, sep='\n', end='\n\n')
+
+except Exception as e:
+    log(e, wait=True)
+
+finally:
+    input()
+    try: browser.quit()
+    except: pass
